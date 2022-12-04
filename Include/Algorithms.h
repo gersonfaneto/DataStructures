b71649a66a42399fbb25ef7ff913eb7646e76467
Gerson Ferreira <gersonferreiradosanjosneto@gmail.com>
2022-12-04 18:47:43 -0300

    feat(*): Add Quick Includes (Algorithms/DataStructures)

---
+++ Include/Algorithms.h
@@ -0,0 +1,9 @@
+#ifndef ALGORITHMS_H
+#define ALGORITHMS_H
+
+#include "../Algorithms/ShortestPath/BellmanFord/BellmanFord.h"
+#include "../Algorithms/ShortestPath/Dijkstra/Dijkstra.h"
+#include "../Algorithms/ShortestPath/DAG/DAG.h"
+
+#endif
+
---
+++ Include/DataStructures.h
@@ -0,0 +1,13 @@
+#ifndef DATASTRUCTURES_H
+#define DATASTRUCTURES_H
+
+#include "../DataStructures/LinkedList/LinkedList.h"
+#include "../DataStructures/Queues/Queue/Queue.h"
+#include "../DataStructures/Queues/PriorityQueue/PriorityQueue.h"
+#include "../DataStructures/Stack/Stack.h"
+#include "../DataStructures/Hash/Hash.h"
+#include "../DataStructures/Trees/BinaySearchTree/BinaySearchTree.h"
+#include "../DataStructures/Trees/AVLTree/AVLTree.h"
+#include "../DataStructures/Graph/Graph.h"
+
+#endif
